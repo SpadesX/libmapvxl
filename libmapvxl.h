5eef27614d93325e2207a33b683c921d74d120e2
lancilloty <99379979+lancilloty@users.noreply.github.com>
2022-08-12 02:39:35 -0400

    Documentation rectifications. (#1)
    * Added code of conduct.
    
    * Added contribution guide.
    
    * Created README.md.
    
    * Small grouping to increase readability.
    
    * Added compilation step to README.md.
    
    * Update README.md
    
    * Update README.md
    
    * Typo fix.
    
    * Removed indent in libmapvxl.h.

--- libmapvxl.h
+++ libmapvxl.h
@@ -6,22 +6,25 @@
 #define DEFAULT_COLOR 0xFF674028
 
 typedef struct {
-    unsigned short MAP_X_MAX;
-    unsigned short MAP_Y_MAX;
-    unsigned short MAP_Z_MAX;
-    unsigned char ***blocks;
-    unsigned int ***color;
+	unsigned short MAP_X_MAX;
+	unsigned short MAP_Y_MAX;
+	unsigned short MAP_Z_MAX;
+	unsigned char ***blocks;
+	unsigned int ***color;
 } MapVxl;
 
 void mapvxlCreate(MapVxl *map, int maxX, int maxY, int maxZ);
 void mapvxlFree(MapVxl *map);
 void mapvxlLoadVXL(MapVxl *map, unsigned char *v);
-unsigned char mapvxlIsSurface(MapVxl *map, int x, int y, int z);
-void mapvxlSetColor(MapVxl *map, int x, int y, int z, unsigned int c);
 size_t mapvxlWriteMap(MapVxl *map, unsigned char *mapOut);
-unsigned int mapvxlGetColor(MapVxl *map, int x, int y, int z);
-void mapvxlSetAir(MapVxl *map, int x, int y, int z);
-unsigned char mapvxlIsSolid(MapVxl *map, int x, int y, int z);
+
 unsigned char mapvxlFindTopBlock(MapVxl *map, int x, int y);
+
+void mapvxlSetColor(MapVxl *map, int x, int y, int z, unsigned int c);
+unsigned int mapvxlGetColor(MapVxl *map, int x, int y, int z);
+
+void mapvxlSetAir(MapVxl *map, int x, int y, int z);
+unsigned char mapvxlIsSurface(MapVxl *map, int x, int y, int z);
+unsigned char mapvxlIsSolid(MapVxl *map, int x, int y, int z);
 
 #endif
