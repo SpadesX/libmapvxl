c26f4773a2596ea34b9c976e67be9cdfd7b72025
circumscriptor <circumscriptor@protonmail.com>
2022-09-25 13:02:35 +0200

    libmapvxl: Refactor and format code
    - fix top block finding for z > 64
    - add assertions
    - change argument types
    - add _t to block and map structure types

--- libmapvxl.h
+++ libmapvxl.h
@@ -2,46 +2,43 @@
 #define LIBMAPVXL_H
 
 #include <stddef.h>
+#include <stdint.h>
 
-#define DEFAULT_COLOR 0xFF674028
+#define MAPVXL_DEFAULT_COLOR 0xFF674028
 
-typedef union {
-  struct {
-    unsigned char b;
-    unsigned char g;
-    unsigned char r;
-    unsigned char data;
-  };
-  unsigned int raw;
-} Block;
+typedef union mapvxl_block {
+    struct {
+        uint8_t b;
+        uint8_t g;
+        uint8_t r;
+        uint8_t data;
+    };
+    uint32_t raw;
+} mapvxl_block_t;
 
-_Static_assert(sizeof(Block) == 4, "struct Block has invalid size");
+_Static_assert(sizeof(mapvxl_block_t) == 4, "invalid size");
 
-typedef struct {
-  unsigned short MAP_X_MAX;
-  unsigned short MAP_Y_MAX;
-  unsigned short MAP_Z_MAX;
+typedef struct mapvxl {
+    uint16_t          size_x;
+    uint16_t          size_y;
+    uint16_t          size_z;
+    mapvxl_block_t*** blocks;
 
-  Block ***blocks;
+    // flat buffers
+    mapvxl_block_t*** memory_blocks_pp;
+    mapvxl_block_t**  memory_blocks_p;
+    mapvxl_block_t*   memory_blocks;
+} mapvxl_t;
 
-  // flat buffers
-  Block ***memory_blocks_pp;
-  Block **memory_blocks_p;
-  Block *memory_blocks;
-} MapVxl;
-
-void mapvxlCreate(MapVxl *map, int maxX, int maxY, int maxZ);
-void mapvxlFree(MapVxl *map);
-void mapvxlLoadVXL(MapVxl *map, unsigned char *v);
-size_t mapvxlWriteMap(MapVxl *map, unsigned char *mapOut);
-
-unsigned char mapvxlFindTopBlock(MapVxl *map, int x, int y);
-
-void mapvxlSetColor(MapVxl *map, int x, int y, int z, unsigned int c);
-unsigned int mapvxlGetColor(MapVxl *map, int x, int y, int z);
-
-void mapvxlSetAir(MapVxl *map, int x, int y, int z);
-unsigned char mapvxlIsSurface(MapVxl *map, int x, int y, int z);
-unsigned char mapvxlIsSolid(MapVxl *map, int x, int y, int z);
+void     mapvxl_create(mapvxl_t* map, uint16_t size_x, uint16_t size_y, uint16_t size_z);
+void     mapvxl_free(mapvxl_t* map);
+void     mapvxl_read(mapvxl_t* map, uint8_t* src);
+size_t   mapvxl_write(mapvxl_t* map, uint8_t* out);
+uint8_t  mapvxl_find_top_block(mapvxl_t* map, uint16_t x, uint16_t y);
+void     mapvxl_set_color(mapvxl_t* map, uint16_t x, uint16_t y, uint16_t z, uint32_t c);
+uint32_t mapvxl_get_color(mapvxl_t* map, uint16_t x, uint16_t y, uint16_t z);
+void     mapvxl_set_air(mapvxl_t* map, uint16_t x, uint16_t y, uint16_t z);
+int      mapvxl_is_surface(mapvxl_t* map, uint16_t x, uint16_t y, uint16_t z);
+int      mapvxl_is_solid(mapvxl_t* map, uint16_t x, uint16_t y, uint16_t z);
 
 #endif
